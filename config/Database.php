bf295e05393a50830ccfba41c3a27481b3407d27
Widad Istiqomah <18218011@std.stei.itb.ac.id>
2021-01-02 21:25:12 +0700

    fix: db

--- config/Database.php
+++ config/Database.php
@@ -4,11 +4,11 @@
         //DB Params
 
         // DB for development
-        private $host = 'localhost';
-        private $db_name = 'sampahku';
-        private $username = 'root';
-        private $password = '';
-        private $conn;
+        // private $host = 'localhost';
+        // private $db_name = 'sampahku';
+        // private $username = 'root';
+        // private $password = '';
+        // private $conn;
 
         // // DB for deployment
         // private $host = 'sql12.freemysqlhosting.net';
@@ -22,11 +22,15 @@
             $this->conn = null;
 
             try {
-                $this->conn = new PDO(
-                    'mysql:host='.$this->host.';dbname='.$this->db_name,
-                    $this->username,
-                    $this->password
-                );
+                $db = parse_url(getenv("postgres://tsksabxokdawqm:03f55d605a04fa4d25ee18b978c211953141532f2196333a6a7ed0e4862d0744@ec2-52-201-55-4.compute-1.amazonaws.com:5432/d4h2ac7l9anakb"));
+                $this->conn = new PDO("pgsql:" . sprintf(
+                    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+                    $db["host"],
+                    $db["port"],
+                    $db["user"],
+                    $db["pass"],
+                    ltrim($db["path"], "/")
+                ));
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 echo 'Connection Error: '.$e ->getMessage();
